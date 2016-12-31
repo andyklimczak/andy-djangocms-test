cdf3f98ce2e6766dfb3b29ccbc69667a4656c80f
Andy Klimczak <andyklimczak@fastmail.com>
2016-12-31 14:57:24 -0500

    s3 storage

--- djangocms_test/settings.py
+++ djangocms_test/settings.py
@@ -67,9 +67,23 @@ USE_L10N = False
 
 USE_TZ = True
 
+AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
+AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.8/howto/static-files/
+# Tell django-storages that when coming up with the URL for an item in S3 storage, keep
+# it simple - just use this domain plus the path. (If this isn't set, things get complicated).
+# This controls how the `static` template tag from `staticfiles` gets expanded, if you're using it.
+# We also use it in the next setting.
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+
+# This is used by the `static` template tag from `static`, if you're using that. Or if anything else
+# refers directly to STATIC_URL. So it's safest to always set it.
+STATIC_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
+
+# Tell the staticfiles app to use S3Boto storage when writing the collected static files (when
+# you run `collectstatic`).
+STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
@@ -80,34 +94,43 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'djangocms_test', 'static'),
 )
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+STATICFILES_LOCATION = 'static'
+STATICFILES_STORAGE = 'djangocms_test.custom_storages.StaticStorage'
+STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, STATICFILES_LOCATION)
+
+MEDIAFILES_LOCATION = 'media'
+MEDIA_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
+DEFAULT_FILE_STORAGE = 'djangocms_test.custom_storages.MediaStorage'
+
+
 SITE_ID = 1
 
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'djangocms_test', 'templates'),],
-        'OPTIONS': {
-            'context_processors': [
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-                'django.core.context_processors.i18n',
-                'django.core.context_processors.debug',
-                'django.core.context_processors.request',
-                'django.core.context_processors.media',
-                'django.core.context_processors.csrf',
-                'django.core.context_processors.tz',
-                'sekizai.context_processors.sekizai',
-                'django.core.context_processors.static',
-                'cms.context_processors.cms_settings'
-            ],
-            'loaders': [
-                'django.template.loaders.filesystem.Loader',
-                'django.template.loaders.app_directories.Loader',
-                'django.template.loaders.eggs.Loader'
-            ],
-        },
+	'BACKEND': 'django.template.backends.django.DjangoTemplates',
+	'DIRS': [os.path.join(BASE_DIR, 'djangocms_test', 'templates'),],
+	'OPTIONS': {
+	    'context_processors': [
+		'django.contrib.auth.context_processors.auth',
+		'django.contrib.messages.context_processors.messages',
+		'django.core.context_processors.i18n',
+		'django.core.context_processors.debug',
+		'django.core.context_processors.request',
+		'django.core.context_processors.media',
+		'django.core.context_processors.csrf',
+		'django.core.context_processors.tz',
+		'sekizai.context_processors.sekizai',
+		'django.core.context_processors.static',
+		'cms.context_processors.cms_settings'
+	    ],
+	    'loaders': [
+		'django.template.loaders.filesystem.Loader',
+		'django.template.loaders.app_directories.Loader',
+		'django.template.loaders.eggs.Loader'
+	    ],
+	},
     },
 ]
 
@@ -155,7 +178,8 @@ INSTALLED_APPS = (
     'djangocms_googlemap',
     'djangocms_video',
     'djangocms_test',
-    'light_gallery'
+    'light_gallery',
+    'storages',
 )
 
 LANGUAGES = (
@@ -166,18 +190,18 @@ LANGUAGES = (
 CMS_LANGUAGES = {
     ## Customize this
     1: [
-        {
-            'code': 'en',
-            'hide_untranslated': False,
-            'name': gettext('en'),
-            'redirect_on_fallback': True,
-            'public': True,
-        },
+	{
+	    'code': 'en',
+	    'hide_untranslated': False,
+	    'name': gettext('en'),
+	    'redirect_on_fallback': True,
+	    'public': True,
+	},
     ],
     'default': {
-        'hide_untranslated': False,
-        'redirect_on_fallback': True,
-        'public': True,
+	'hide_untranslated': False,
+	'redirect_on_fallback': True,
+	'public': True,
     },
 }
 
@@ -193,7 +217,7 @@ CMS_PLACEHOLDER_CONF = {}
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=config('DATABASE_URL')
+	default=config('DATABASE_URL')
     )
 }
 
